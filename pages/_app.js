21b7bf77d5dc718e5aa8c90d82b628b4a385beab
Jared <jared@digitalnutt.com>
2020-01-21 14:51:25 -0800

    the env variables are not available there

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,11 @@ import Layout from '../components/Layout';
 import { Auth0Provider } from 'use-auth0-hooks';
 
 const App = ({ Component, pageProps }) => {
-  const { AUTH0_CLIENT_ID, AUTH0_DOMAIN, AUTH0_REDIRECT_URL } = process.env;
   return (
     <Auth0Provider
-      domain={AUTH0_DOMAIN}
-      clientId={AUTH0_CLIENT_ID}
-      redirectUri={AUTH0_REDIRECT_URL}>
+      domain={process.env.AUTH0_DOMAIN}
+      clientId={process.env.AUTH0_CLIENT_ID}
+      redirectUri={process.env.AUTH0_REDIRECT_URL}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
