d9e4f045f6bc9fec879cbc558afeb03940c801f8
Jared <jared@digitalnutt.com>
2020-01-30 12:58:50 -0800

    added api

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import * as queries from '../src/graphql/queries';
 
 const Index = () => {
+  const data = API.graphql(graphqlOperation(queries.listTasks)).then(data => {
+    console.log(data);
+  });
   return (
     <section className="section">
       <h1 className="title">Welcome to kurikku</h1>
